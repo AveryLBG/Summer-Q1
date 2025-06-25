8d1f2f382aad1a96b7898e9427fa93f4f6c76223
Avery Garner <avery.garner@hgs.hiddengeniusproject.org>
2025-06-25 13:37:14 -0700

    3 Prompts

--- Python/Week2/UserInput.py
+++ Python/Week2/UserInput.py
@@ -1,3 +1,4 @@
-print("What is your name?")
-name = input()
-print("Hello, " + name)
+name = input("What is your name?")
+age = input("What is your age? ")
+grade = input("What grade are you in? (just a number will work) ")
+print( "Hello, " + name + ", you are " + age + " years old and in grade " + grade + "." )
