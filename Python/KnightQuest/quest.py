c43a5deec6896752d36194bc27aac55936c40709
Avery Garner <avery.garner@hgs.hiddengeniusproject.org>
2025-07-24 13:04:11 -0700

    KQ Player

--- Python/KnightQuest/quest.py
+++ Python/KnightQuest/quest.py
@@ -1,27 +1,47 @@
 import pgzrun
 
 
-GRID_WIDTH = 16 #game width in tiles
-GRID_HEIGHT = 12 #game height in tiles
+GRID_WIDTH = 28 #game width in tiles OG WIDTH = 16
+GRID_HEIGHT = 17 #game height in tiles OG HEIGHT = 12
 GRID_SIZE = 50 # the size of each tile in pixels
 
 WIDTH = GRID_WIDTH * GRID_SIZE # The width of a tile
 HEIGHT = GRID_HEIGHT * GRID_SIZE # the height of a tile
 
-MAP = [
-    'WWWWWWWWDWWWWWWW',
-    'W              W',
-    'W       K      W',
-    'W       G      W',
-    'WG  WWWWWWWW  GW',
-    'W              W',
-    'W       K      W',
-    'W   WWWWWWWW   W',
-    'W              W',
-    'W              W',
-    'W      P       W',
-    'WWWWWWWWWWWWWWWW',
-]
+MAP = [  
+    'WWWWWWWWWWWWWWDWWWWWWWWWWWWW',            
+    'W                          W',
+    'W                          W',
+    'W                          W',
+    'W    WWWWWWWW K WWWWWWWW   W',
+    'W                          W',
+    'W    G                 G   W',
+    'W    WWWWWWWWWWWWWWWWWWW   W',
+    'W                          W',
+    'W                          W',
+    'W                          W',
+    'W                          W',
+    'W                          W',
+    'W                          W',
+    'W          W    W          W',
+    'W       G  W  P W  G       W',
+    'WWWWWWWWWWWWWWWWWWWWWWWWWWWW',]
+
+
+#OG MAP =[
+#    'WWWWWWWWDWWWWWWW',
+#    'W              W',
+#    'W       K      W',
+#    'W       G      W',
+#    'WG  WWWWWWWW  GW',
+#    'W              W',
+#    'W       K      W',
+#    'W   WWWWWWWW   W',
+#    'W              W',
+#    'W              W',
+#    'W      P       W',
+#    'WWWWWWWWWWWWWWWW',
+#]
 #Converts grid position to screen coordinates
 def GetScreenCoords(x, y):
     return(x * GRID_SIZE, y * GRID_SIZE)
@@ -34,7 +54,7 @@ def DrawBackground():
 
 def SetupGame():
     global player #define player as global
-    player = pgzrun.Actor("player", anchor=("left", "top")) #Create an actor (moving object), and set its anchor (idk)
+    player = Actor("player", anchor=("left", "top")) #Create an actor (moving object), and set its anchor (idk)
     for y in range(GRID_HEIGHT):
         for x in range(GRID_WIDTH):
             square = MAP[y][x]#Gets the character from the MAP var
@@ -51,10 +71,38 @@ def DrawScenery():
             elif square == "D":
                 screen.blit("door", GetScreenCoords(x, y))
 
+def GetActorGridPos(actor): #gets the actor as an argument and finds its position on the grid
+    return (round(actor.x/GRID_SIZE)), (round(actor.y/GRID_SIZE)) #divides pixel coords by grid conversion number
+
+def DrawActors():#Draw entities
+    player.draw()
+
 def draw(): #Draws everything USE 'draw', not 'Draw', 'draw' is built in
+    screen.clear()
     DrawBackground()
     DrawScenery()
-    SetupGame()
+    DrawActors()
 
+def MovePlayer(dx, dy):
+    (x, y) = GetActorGridPos(player) #gets the player position
+    x += dx #adds the inputted move to the current coords
+    y += dy #adds the inputted move to the current coords
+    square = MAP[y][x]#Makes sure the player doesn't go through a wall/door
+    if square == "W": 
+        return
+    elif square == "D":
+        return
+    player.pos = GetScreenCoords(x,y) #Prints the player at their new coordinate
 
+def on_key_down(key):
+    if key == keys.LEFT:
+        MovePlayer(-1, 0)
+    elif key == keys.UP:
+        MovePlayer(0, -1)
+    elif key == keys.RIGHT:
+        MovePlayer(1, 0)
+    elif key == keys.DOWN:
+        MovePlayer(0, 1)
+
+SetupGame()
 pgzrun.go()
