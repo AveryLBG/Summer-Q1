3ff6a7fd220e8b8be447cdd832a8b373cb721e9c
Avery Garner <avery.garner@hgs.hiddengeniusproject.org>
2025-07-31 15:37:52 -0700

    Flourished KQ

--- Python/KnightQuest/quest.py
+++ Python/KnightQuest/quest.py
@@ -8,13 +8,104 @@ GRID_WIDTH = 27 #game width in tiles OG WIDTH = 16
 GRID_HEIGHT = 17 #game height in tiles OG HEIGHT = 12
 GRID_SIZE = 50 # the size of each tile in pixels
 GUARDMOVEINTERVAL = 0.25 #The interval at which each guard moves
-PLAYERMOVEINTERVAL = 0.1
+PLAYERMOVEINTERVAL = 0.001
 
 WIDTH = GRID_WIDTH * GRID_SIZE # The width of a tile
 HEIGHT = GRID_HEIGHT * GRID_SIZE # the height of a tile
 BACKGROUND_SEED = 12345
 MAPS = [
+    
+     [
+    'WWWWWWWWWWWWWDWWWWWWWWWWWWW',            
+    'W           ! !           W',
+    'W           ! !           W',
+    'W           ! !           W',
+    'W           ! !           W',
+    'W           !K!           W',
+    'W           ! !           W',
+    'W           ! !           W',
+    'W           ! !           W',
+    'W           ! !           W',
+    'WG          ! !          GW',
+    'WG          ! !          GW',
+    'WG          ! !         GGW',
+    'WG          ! !          GW',
+    'WG          ! !          GW',
+    'WG          !P!          GW',
+    'WWWWWWWWWWWWWWWWWWWWWWWWWWW',],
     [
+    'WWWWWWWWWWWWWDWWWWWWWWWWWWW',            
+    'W                      G  W',
+    'W                         W',
+    'W                         W',
+    'W              G          W',
+    'W  G                      W',
+    'W                  ???K   W',
+    'W                  ?      W',
+    'W                  ?  ?   W',
+    'W  K???????????????????   W',
+    'W             ?           W',
+    'W     G       ?           W',
+    'W      G      ?       G   W',
+    'W            ?P???        W',
+    'W       G     ?  ?        W',
+    'W  G          ???????????KW',
+    'WWWWWWWWWWWWWWWWWWWWWWWWWWW',],
+    [
+    'WWWWWWWWWWWWWWWWWWWWWWWWWWW',            
+    'WP  WK                    W',
+    'W   W   WWWWWWWW          W',
+    'W ??W   WK                W',
+    'W   WW  W      ?          W',
+    'W  WGW  W      ?          W',
+    'W  W W  W      ?          W',
+    'W       W WWWWW?          W',
+    'W  W    W     G?          W',
+    'WWWWWWWWWWWWWWWWWWWWWWW   W',
+    'WG?                   WWW W',
+    'WG? ?                   G W',
+    'WG? ?                  WWWW',
+    'WG? ?                     W',
+    'WG? ?                     W',
+    'WK  ?                     D',
+    'WWWWWWWWWWWWWWWWWWWWWWWWWWW',],
+    [
+    'WWWWWWWWWWWWWDWWWWWWWWWWWWW',            
+    'WGGGGGGGGGGGGGGGGGGGGGGGGGW',
+    'WGGGGGGGGGGGGKGGGGGGGGGGGGW',
+    'WGGGGGGGGGGGGGGGGGGGGGGGGGW',
+    'WGGGGGGGGGGGGGGGGGGGGGGGGGW',
+    'WGGGGGGGGGGGGGGGGGGGGGGGGGW',
+    'WG                    K  GW',
+    'W                    !!!!!W',
+    'W!!!!!!                   W',
+    'W  K  !                   W',
+    'W     !                   W',
+    'W     !                   W',
+    'W?????????????????????????W',
+    'W             P           W',
+    'W                         W',
+    'W                        KW',
+    'WWWWWWWWWWWWWWWWWWWWWWWWWWW',],
+    [
+    'WWWWWWWWWWWWWDWWWWWWWWWWWWW',            
+    'WWWWWWWWWWWW   WWWWWWWWWWWW',
+    'W                         W',
+    'W  ?????????????????????  W',
+    'W  ?                   ?  W',
+    'W W?                   ?W W',
+    'W  ?                   ?  W',
+    'WW ?                   ? WW',
+    'W  ?                   ?  W',
+    'WKG?                   ?GKW',
+    'WWWWWWWWWWWWW WWWWWWWWWWWWW',
+    'W                         W',
+    'W                         W',
+    'W                         W',
+    'W                         W',
+    'W            P            W',
+    'WWWWWWWWWWWWWWWWWWWWWWWWWWW',],
+[
     'WWWWWWWWWWWWWDWWWWWWWWWWWWW',            
     'W                         W',
     'W                         W',
@@ -34,43 +125,26 @@ MAPS = [
     'WWWWWWWWWWWWWWWWWWWWWWWWWWW',],
     [
     'WWWWWWWWWWWWWDWWWWWWWWWWWWW',            
-    'WGGGGGGGGGGGGGGGGGGGGGGGGGW',
-    'WG           K           GW',
-    'WG                       GW',
-    'WG                       GW',
-    'WG                       GW',
-    'WG                    K  GW',
-    'W                         W',
-    'W                         W',
-    'W  K                      W',
-    'W                         W',
-    'W                         W',
-    'W                         W',
-    'W             P           W',
-    'W                         W',
-    'W                        KW',
-    'WWWWWWWWWWWWWWWWWWWWWWWWWWW',],
-    [
-    'WWWWWWWWWWWWWDWWWWWWWWWWWWW',            
-    'WWWWWWWWWWWW   WWWWWWWWWWWW',
-    'WKGW                   WGKW',
-    'WGGW                   WGGW',
-    'WGGW                   WGGW',
-    'WGGW                   WGGW',
-    'WGGW                   WGGW',
-    'WGGW                   WGGW',
-    'W                         W',
-    'WGGW                   WGGW',
-    'WWWWWWWWWWWWW WWWWWWWWWWWWW',
     'W                         W',
     'W                         W',
     'W                         W',
     'W                         W',
-    'W            P            W',
+    'W  GWWWWWWWW   WWWWWWWWG  W',
+    'W                         W',
+    'W WWWWWWWWWWWWWWWWWWWWWWW W',
+    'W W                     W W',
+    'W W                     W W',
+    'WGW          K          WGW',
+    'W W                     W W',
+    'W W                     W W',
+    'W W                     W W',
+    'W WWWWWWWWWW   WWWWWWWWWW W',
+    'WK           P           KW',
     'WWWWWWWWWWWWWWWWWWWWWWWWWWW',],
 
 ]
 MAP = MAPS[level]
+
 
     
 #OG MAP =[
@@ -93,12 +167,14 @@ def GetScreenCoords(x, y):
 
 #This draws the floor as a background
 def DrawBackground():
+    random.seed(BACKGROUND_SEED)
     for y in range (GRID_HEIGHT): #loops through each row
         for x in range(GRID_WIDTH): #loops through each column
             if x % 2 == y % 2:
-                screen.blit("floor1", GetScreenCoords(x, y)) #Draws the image at the given position
+                screen.blit("floor1_grayscale", GetScreenCoords(x, y)) #Draws the image at the given position
             else:
-                screen.blit("floor2", GetScreenCoords(x, y)) #Draws the image at the given position
+                screen.blit("floor2_grayscale", GetScreenCoords(x, y)) #Draws the image at the given position
+
 
 def SetupGame():
     global player #define player as global
@@ -110,7 +186,7 @@ def SetupGame():
     global level
 
 
-    player = Actor("player", anchor=("left", "top")) #Create an actor for any moving objects player
+    player = Actor("player1", anchor=("left", "top")) #Create an actor for any moving objects player
     keysToCollect = []
     guards = []
     gameOver = False
@@ -138,10 +214,19 @@ def DrawScenery():
     for y in range(GRID_HEIGHT):
         for x in range(GRID_WIDTH):
             square = MAP[y][x]
+            n = random.randint(0, 99) #Chooses a number between 0 and 99
             if square == "W":
                 screen.blit("wall", GetScreenCoords(x, y))
+                if n < 7:
+                    screen.blit('crack1', GetScreenCoords(x, y))
+                elif n < 15:
+                    screen.blit('crack2', GetScreenCoords(x, y))
             elif square == "D":
                 screen.blit("door", GetScreenCoords(x, y))
+            elif square == "?":
+                screen.blit("gonly_wall", GetScreenCoords(x, y))
+            elif square == "!":
+                screen.blit("hole", GetScreenCoords(x, y))
 
 def GetActorGridPos(actor): #gets the actor as an argument and finds its position on the grid
     return (round(actor.x/GRID_SIZE)), (round(actor.y/GRID_SIZE)) #divides pixel coords by grid conversion number
@@ -168,11 +253,7 @@ def MovePlayer(dx, dy):
     global gameOver
     global playerWon
     global img2
-    Pframe = random.randrange(1,3)
-    if Pframe == 1:
-        player.image = "player"
-    else:
-        player.image = "player2"
+
     if gameOver: #if the game is over
         #stop the player from moving by breaking the function
         return
@@ -181,6 +262,8 @@ def MovePlayer(dx, dy):
     y += dy #adds the inputted move to the current coords
     square = MAP[y][x]#Makes sure the player doesn't go through a wall/door
     if square == "W": # if the player tries to move into a wall, don't let the player through
+        return
+    elif square == "!":
         return
     elif square == "D": # if the list of keys left to collect, there are keys left to collect, don't let the player through.
         if  len(keysToCollect) > 0:
@@ -193,7 +276,7 @@ def MovePlayer(dx, dy):
         if x ==keyX and y == keyY: #Checks if the player is touching the key
             keysToCollect.remove(key)
             break
-        #animate(player, pos=GetScreenCoords(x,y), duration=PLAYERMOVEINTERVAL)
+        #animate(player, pos=GetScreenCoords(x,y), duration=PLAYERMOVEINTERVAL/5)
     player.pos = GetScreenCoords(x,y) #Prints the player at their new coordinate
     
 
@@ -211,24 +294,29 @@ def on_key_down(key):
             DrawActors()
             SetupGame()
             
-    if key == keys.LEFT:
+    if key == keys.LEFT or key == keys.A:
         MovePlayer(-1, 0)
-    elif key == keys.UP:
+        player.image = 'player3'
+    elif key == keys.UP or key == keys.W:
         MovePlayer(0, -1)
-    elif key == keys.RIGHT:
+        player.image = 'player2'
+    elif key == keys.RIGHT or key == keys.D:
         MovePlayer(1, 0)
-    elif key == keys.DOWN:
+        player.image = 'player4'
+    elif key == keys.DOWN or key == keys.S:
         MovePlayer(0, 1)
+        player.image = 'player1'
 
 def DrawGameOver():
     #Calculate and store the middle pos of the screen
     screenMiddle = (WIDTH/2, HEIGHT/2)
     #Draw game over
-    screen.draw.text("Game Over", midbottom = screenMiddle, fontsize = GRID_SIZE*2, color="cyan", owidth=1)
     if playerWon:
-        screen.draw.text("YOU WIN", midtop = screenMiddle, fontsize = GRID_SIZE*2, color="green", owidth=1)
+        screen.draw.text("YOU WIN", midbottom = screenMiddle, fontsize = GRID_SIZE*2, color="green", owidth=1)
+        screen.draw.text("continue?", midtop = screenMiddle, fontsize = GRID_SIZE*2, color="white", owidth=1)
     else:
-        screen.draw.text("YOU LOSE", midtop = screenMiddle, fontsize = GRID_SIZE*2, color="red", owidth=1)
+        screen.draw.text("YOU LOSE", midbottom = screenMiddle, fontsize = GRID_SIZE*2, color="red", owidth=1)
+        screen.draw.text("try again?", midtop = screenMiddle, fontsize = GRID_SIZE*2, color="white", owidth=1)
 def MoveGuard(guard):
     global gameOver
     if gameOver:
@@ -238,18 +326,22 @@ def MoveGuard(guard):
     (guardX, guardY) = GetActorGridPos(guard)
     
     #Check which direction the player is, and if there is a wall in the way
-    if playerX > guardX and MAP[guardY][guardX+1] != 'W':
+    if playerX > guardX and MAP[guardY][guardX+1] != 'W' and MAP[guardY][guardX+1] != '?':
         guardX += 1
-    if playerX < guardX and MAP[guardY][guardX-1] != 'W':
+        guard.image = 'guard3'
+    if playerX < guardX and MAP[guardY][guardX-1] != 'W' and MAP[guardY][guardX-1] != '?':
         guardX -= 1
-    if playerY > guardY and MAP[guardY+1][guardX] != 'W':
+        guard.image = 'guard2'
+    if playerY > guardY and MAP[guardY+1][guardX] != 'W' and MAP[guardY+1][guardX] != '?':
         guardY += 1
-    if playerY < guardY and MAP[guardY-1][guardX] != 'W':
+        guard.image = 'guard'
+    if playerY < guardY and MAP[guardY-1][guardX] != 'W' and MAP[guardY-1][guardX] != '?':
         guardY -= 1
+        guard.image = 'guard1'
 
     
     #update the guard position on screen and animate him
-    animate(guard, pos=GetScreenCoords(guardX, guardY), duration=GUARDMOVEINTERVAL/3)
+    animate(guard, pos=GetScreenCoords(guardX, guardY), duration=GUARDMOVEINTERVAL/2)
     #guard.pos = GetScreenCoords(guardX, guardY)
     #Check if the guard and player are in the same position
     if guardX == playerX and guardY == playerY:
@@ -257,10 +349,15 @@ def MoveGuard(guard):
 
 def MoveGuards():
     global GUARDMOVEINTERVAL
-    global staggerchance
-    staggerchance = random.randrange(1,7)
+    gframe = 1
     for guard in guards:
         MoveGuard(guard)
+        if gframe == 1:
+            gframe = 0
+            guard.image
+        else:
+            gframe = 1
+
         
         
        
@@ -268,6 +365,5 @@ def MoveGuards():
 
 
 SetupGame()
-pgzrun.go()
 clock.schedule_interval(MoveGuards, GUARDMOVEINTERVAL)
 pgzrun.go()
